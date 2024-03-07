146c6281395bd6f838696acbf8896ab35200673f
hemaksh18 <132896908+hemaksh18@users.noreply.github.com>
2024-03-07 22:37:38 +0530

    Update and rename parallel.cpp to parallelopenMp.cpp

---
+++ parallelopenMp.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <cstdlib> 
+#include <ctime>   
+#include <omp.h>   
+
+using namespace std;
+using namespace std::chrono;
+
+int dividing(vector<int>& arr, int low, int high) {
+    int rightEnd = arr[high]; 
+    int i = low - 1; 
+
+    #pragma omp parallel for shared(arr, i)
+    for (int j = low; j < high; j++) {
+        
+        if (arr[j] < rightEnd) {
+            #pragma omp critical
+            {
+                i++; 
+                swap(arr[i], arr[j]); 
+            }
+        }
+    }
+    swap(arr[i + 1], arr[high]); 
+    return i + 1; 
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) { 
+       
+        int pi = dividing(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    srand(time(0)); 
+
+    
+    vector<int> arr;
+    const int SIZE = 1000; 
+    for (int i = 0; i < SIZE; ++i) {
+        arr.push_back(rand() % 100); 
+    }
+
+    auto start = high_resolution_clock::now();
+
+    quickSort(arr, 0, SIZE - 1);
+
+    auto stop = high_resolution_clock::now();
+
+   
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by Quick Sort: " << duration.count() << " microseconds" << endl;
+
+    return 0;
+}
