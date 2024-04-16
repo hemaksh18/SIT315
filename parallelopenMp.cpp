5c4e1ab0ef6750efb8478ec35bf93f564531c69a
hemaksh18 <132896908+hemaksh18@users.noreply.github.com>
2024-04-16 15:20:28 +0530

    Update parallelopenMp.cpp

--- parallelopenMp.cpp
+++ parallelopenMp.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 #include <chrono>
 #include <cstdlib> 
-#include <ctime>   
-#include <omp.h>   
+#include <ctime>  
+#include <thread>
 
 using namespace std;
 using namespace std::chrono;
@@ -12,34 +12,35 @@ int dividing(vector<int>& arr, int low, int high) {
     int rightEnd = arr[high]; 
     int i = low - 1; 
 
-    #pragma omp parallel for shared(arr, i)
     for (int j = low; j < high; j++) {
-        
         if (arr[j] < rightEnd) {
-            #pragma omp critical
-            {
-                i++; 
-                swap(arr[i], arr[j]); 
-            }
+            i++;
+            swap(arr[i], arr[j]); 
         }
     }
     swap(arr[i + 1], arr[high]); 
     return i + 1; 
 }
 
-void quickSort(vector<int>& arr, int low, int high) {
+void quickSort(vector<int>& arr, int low, int high, int chunk_size) {
     if (low < high) { 
-       
         int pi = dividing(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+
+        if (high - low < chunk_size) {
+            quickSort(arr, low, pi - 1, chunk_size);
+            quickSort(arr, pi + 1, high, chunk_size);
+        } else {
+            thread left_thread(quickSort, ref(arr), low, pi - 1, chunk_size);
+            thread right_thread(quickSort, ref(arr), pi + 1, high, chunk_size);
+
+            left_thread.join();
+            right_thread.join();
+        }
     }
 }
 
 int main() {
     srand(time(0)); 
-
-    
     vector<int> arr;
     const int SIZE = 1000; 
     for (int i = 0; i < SIZE; ++i) {
@@ -48,11 +49,13 @@ int main() {
 
     auto start = high_resolution_clock::now();
 
-    quickSort(arr, 0, SIZE - 1);
+    // Define chunk size
+    int chunk_size = 10000;
+
+    quickSort(arr, 0, SIZE - 1, chunk_size);
 
     auto stop = high_resolution_clock::now();
 
-   
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Time taken by Quick Sort: " << duration.count() << " microseconds" << endl;
 
